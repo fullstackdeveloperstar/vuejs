57c4b0b753e4e62a34bd89c8d85f65d6d89795b1
calinblitiu <30364734+calinblitiu@users.noreply.github.com>
2018-04-24 23:16:06 -0700

    hotels in the city

---
+++ hotelsinthecity/src/components/GoogleMap.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="google-map" id="testmap"></div>
+</template>
+<script>
+export default {
+  name: 'GoogleMap',
+  props: ['name'],
+  data: function () {
+    return {
+      mapName: this.name + "-map",
+    }
+  },
+
+  mounted: function () {
+    const element = document.getElementById("testmap");
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(51.501527,-0.1921837)
+    }
+    const map = new google.maps.Map(element, options);
+  }
+};
+</script>
+
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: gray;
+}
+
+.gm-style > div:nth-child(10){
+ display:none;
+}
+</style>
